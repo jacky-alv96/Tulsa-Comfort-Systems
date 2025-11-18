b604141435ebeb69ce2d472999ceafe75d434404
jacky-alv96 <spanishwithjacky@gmail.com>
2025-11-18 15:03:44 -0600

    mobile burger menu fixed

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,13 +1,16 @@
 const burger = document.querySelector('.burger')
 const navLinks = document.querySelector('.nav-links')
+const body = document.body
 
 const navSlideIn = () => {
   navLinks.style.transition = "0.8s ease"
   navLinks.style.transform = "translateY(0%)"
+  body.classList.add('menu-open')
 }
 const navSlideOut = () => {
   navLinks.style.transition = "0.8s ease"
   navLinks.style.transform = "translateY(-100%)"
+  body.classList.remove('menu-open')
 }
 
 burger.addEventListener('click', function() {
